72d1520a81766e4a3159ef598957806b2fa59248
laura6790 <lvidmar@upv.edu.es>
2025-10-08 20:08:15 +0200

    Delete g02_prii3_sprint1 directory

--- g02_prii3_sprint1/build/g02_prii3_turtlesim/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/laura/Descargas/g02_prii3_workspace-master(1)/install/g02_prii3_turtlesim'
